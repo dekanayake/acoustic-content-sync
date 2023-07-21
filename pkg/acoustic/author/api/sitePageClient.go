cd86d6e01564390359130dd0f8f91e969eb11c83
dekanayake <dumindae@gmail.com>
2023-07-21 23:25:00 +1000

    Fixed changing the parent sites.

--- pkg/acoustic/author/api/sitePageClient.go
+++ pkg/acoustic/author/api/sitePageClient.go
@@ -3,12 +3,18 @@ package api
 import (
 	"encoding/json"
 	"github.com/dekanayake/acoustic-content-sync/pkg/errors"
+	"github.com/thoas/go-funk"
 	"gopkg.in/resty.v1"
+	"strconv"
 )
 
 type SitePageClient interface {
 	Create(siteID string, sitePage SitePage) (*SitePageResponse, error)
-	GetChildPages(siteID string, parentPageID string) ([]SitePageResponse, error)
+	GetChildPages(siteID string, parentPageID string) ([]string, error)
+	Update(siteID string, pageId string, sitePage SitePage) (*SitePageResponse, error)
+	Move(siteID string, sourcePageID string, sourcePageRev string, targetPageID string, targetPageRev string, targetPagePosition int) (*SitePageResponse, error)
+	Delete(siteID string, pageId string, deleteContent bool) (*SitePageResponse, error)
+	Get(siteID string, pageId string) (*SitePageResponse, error)
 }
 
 type sitePageClient struct {
@@ -23,6 +29,14 @@ func NewSitePageClient(acousticApiUrl string) SitePageClient {
 		c2:             Connect(),
 		acousticApiUrl: acousticApiUrl,
 	}
+}
+
+type childSitePageResponseList struct {
+	Items []childSitePageResponse `json:"items"`
+}
+
+type childSitePageResponse struct {
+	ID string `json:"id"`
 }
 
 func (sitePageClient sitePageClient) Create(siteID string, sitePage SitePage) (*SitePageResponse, error) {
@@ -45,15 +59,107 @@ func (sitePageClient sitePageClient) Create(siteID string, sitePage SitePage) (*
 	}
 }
 
-func (sitePageClient sitePageClient) GetChildPages(siteID string, parentPageID string) ([]SitePageResponse, error) {
+func (sitePageClient sitePageClient) Update(siteID string, pageId string, sitePage SitePage) (*SitePageResponse, error) {
+	req := sitePageClient.c1.NewRequest().SetBody(sitePage).
+		SetResult(&SitePageResponse{}).
+		SetError(&ContentAuthoringErrorResponse{}).SetPathParams(map[string]string{"siteId": siteID, "pageID": pageId}).SetQueryParam("forceOverride", "true")
+
+	if resp, err := req.Put(sitePageClient.acousticApiUrl + "/authoring/v1/sites/{siteId}/pages/{pageID}"); err != nil {
+		return nil, errors.ErrorWithStack(err)
+	} else if resp.IsSuccess() {
+		return resp.Result().(*SitePageResponse), nil
+	} else if resp.IsError() && resp.StatusCode() == 400 {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in creating content : " + resp.Status() + "  " + string(errorString))
+	} else {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in creating content : " + resp.Status() + "  " + string(errorString))
+	}
+}
+
+func (sitePageClient sitePageClient) Move(siteID string, sourcePageID string, sourcePageRev string, targetPageID string, targetPageRev string, targetPagePosition int) (*SitePageResponse, error) {
+	req := sitePageClient.c1.NewRequest().
+		SetResult(&SitePageResponse{}).
+		SetError(&ContentAuthoringErrorResponse{}).SetPathParams(map[string]string{"siteId": siteID}).
+		SetQueryParams(map[string]string{
+			"sourceId":       sourcePageID,
+			"sourceRev":      sourcePageRev,
+			"targetId":       targetPageID,
+			"targetRev":      targetPageRev,
+			"targetPosition": strconv.Itoa(targetPagePosition),
+		})
+
+	if resp, err := req.Put(sitePageClient.acousticApiUrl + "/authoring/v1/sites/{siteId}/pages/move"); err != nil {
+		return nil, errors.ErrorWithStack(err)
+	} else if resp.IsSuccess() {
+		return resp.Result().(*SitePageResponse), nil
+	} else if resp.IsError() && resp.StatusCode() == 400 {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in moving content : " + resp.Status() + "  " + string(errorString))
+	} else {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in moving content : " + resp.Status() + "  " + string(errorString))
+	}
+}
+
+func (sitePageClient sitePageClient) Get(siteID string, pageId string) (*SitePageResponse, error) {
+	req := sitePageClient.c1.NewRequest().
+		SetResult(&SitePageResponse{}).
+		SetError(&ContentAuthoringErrorResponse{}).SetPathParams(map[string]string{"siteId": siteID, "pageId": pageId})
+
+	if resp, err := req.Get(sitePageClient.acousticApiUrl + "/authoring/v1/sites/{siteId}/pages/{pageId}"); err != nil {
+		return nil, errors.ErrorWithStack(err)
+	} else if resp.IsSuccess() {
+		return resp.Result().(*SitePageResponse), nil
+	} else if resp.IsError() && resp.StatusCode() == 400 {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in moving content : " + resp.Status() + "  " + string(errorString))
+	} else {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in moving content : " + resp.Status() + "  " + string(errorString))
+	}
+}
+
+func (sitePageClient sitePageClient) Delete(siteID string, pageId string, deleteContent bool) (*SitePageResponse, error) {
+	req := sitePageClient.c1.NewRequest().
+		SetResult(&SitePageResponse{}).
+		SetError(&ContentAuthoringErrorResponse{}).SetPathParams(map[string]string{"siteId": siteID, "pageId": pageId}).
+		SetQueryParams(map[string]string{
+			"delete-content": strconv.FormatBool(deleteContent),
+		})
+
+	if resp, err := req.Delete(sitePageClient.acousticApiUrl + "/authoring/v1/sites/{siteId}/pages/{pageId}"); err != nil {
+		return nil, errors.ErrorWithStack(err)
+	} else if resp.IsSuccess() {
+		return resp.Result().(*SitePageResponse), nil
+	} else if resp.IsError() && resp.StatusCode() == 400 {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in moving content : " + resp.Status() + "  " + string(errorString))
+	} else {
+		error := resp.Error().(*ContentAuthoringErrorResponse)
+		errorString, _ := json.MarshalIndent(error, "", "\t")
+		return nil, errors.ErrorMessageWithStack("error in moving content : " + resp.Status() + "  " + string(errorString))
+	}
+}
+
+func (sitePageClient sitePageClient) GetChildPages(siteID string, parentPageID string) ([]string, error) {
 	req := sitePageClient.c2.NewRequest().
-		SetResult(&SitePageResponseList{}).
+		SetResult(&childSitePageResponseList{}).
 		SetError(&ContentAuthoringErrorResponse{}).SetPathParams(map[string]string{"siteId": siteID, "parentPageID": parentPageID})
 
 	if resp, err := req.Get(sitePageClient.acousticApiUrl + "/mydelivery/v1/sites/{siteId}/pages/by-parent/{parentPageID}"); err != nil {
 		return nil, errors.ErrorWithStack(err)
 	} else if resp.IsSuccess() {
-		return resp.Result().(*SitePageResponseList).Items, nil
+		return funk.Map(resp.Result().(*childSitePageResponseList).Items, func(x childSitePageResponse) string {
+			return x.ID
+		}).([]string), nil
 	} else if resp.IsError() && resp.StatusCode() == 400 {
 		error := resp.Error().(*ContentAuthoringErrorResponse)
 		errorString, _ := json.MarshalIndent(error, "", "\t")
