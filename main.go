943eb8db63fcce96024dca25f1db44c437a22218
dekanayake <dumindae@gmail.com>
2023-11-23 11:54:34 +1100

    Refactored to use commands

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"flag"
-	"github.com/dekanayake/acoustic-content-sync/pkg/acoustic/csv"
-	"github.com/dekanayake/acoustic-content-sync/pkg/env"
+	"github.com/dekanayake/acoustic-content-sync/pkg/command"
 	"github.com/dekanayake/acoustic-content-sync/pkg/logrus"
 	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func init() {
@@ -28,106 +23,6 @@ func init() {
 	log.SetLevel(log.InfoLevel)
 }
 
-func createOrUpdateContents(feedName string, configName string, acousticContentLib string, contentType string) {
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-	var err error
-	contentService := csv.NewContentUseCase(os.Getenv("AcousticAPIURL"), acousticContentLib)
-	status, err := contentService.CreateBatch(contentType, feedName, configName)
-	log.Info(" total records :" + strconv.Itoa(status.TotalCount()))
-	log.Info(" success created record count  :" + strconv.Itoa(len(status.Success)))
-	if status.FailuresExist() {
-		log.Error("There are " + strconv.Itoa(len(status.Failed)) + " failures in creating contents , please check the log in " + env.ErrorLogFileLocation())
-		status.PrintFailed()
-	}
-	if err != nil {
-		errorHandling.WithError(err).Panic(err)
-	}
-}
-
-func deleteContents(deleteUsingFeed bool, deleteMappingName string, feedName string, configName string, libraryID string, contentType string) {
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-	deleteService := csv.NewDeleteService(env.AcousticAPIUrl())
-	if deleteUsingFeed {
-		status, err := deleteService.DeleteByFeed(deleteMappingName, contentType, feedName, configName)
-		if err != nil {
-			errorHandling.WithError(err).Panic(err)
-		}
-		log.Info(" total records :" + strconv.Itoa(status.TotalCount()))
-		log.Info(" success deleted record count  :" + strconv.Itoa(len(status.Success)))
-		if status.FailuresExist() {
-			log.Error("There are " + strconv.Itoa(len(status.Failed)) + " failures in creating contents , please check the log in " + env.ErrorLogFileLocation())
-			status.PrintFailed()
-		}
-		if err != nil {
-			errorHandling.WithError(err).Panic(err)
-		}
-	} else {
-		err := deleteService.Delete(libraryID, deleteMappingName, configName)
-		if err != nil {
-			errorHandling.WithError(err).Panic(err)
-		}
-	}
-}
-
-func createCategories(catName string, feedName string, configName string) {
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-	catService := csv.NewCategoryService(os.Getenv("AcousticAPIURL"))
-	err := catService.Create(catName, feedName, configName)
-	if err != nil {
-		errorHandling.WithError(err).Panic(err)
-	}
-}
-
-func createSitePages(siteId string, parentPageId string, contentType string, dataFeedPath string, configPath string) {
-	os.Setenv("ParentPageContentTypeID", contentType)
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-	siteUseCase := csv.NewSiteUseCase(os.Getenv("AcousticAPIURL"))
-	status, err := siteUseCase.CreatePages(siteId, parentPageId, contentType, dataFeedPath, configPath)
-	log.Info(" total records :" + strconv.Itoa(status.TotalCount()))
-	log.Info(" success created pages count  :" + strconv.Itoa(len(status.Success)))
-	if status.FailuresExist() {
-		log.Error("There are " + strconv.Itoa(len(status.Failed)) + " failures in creating pages , please check the log in " + env.ErrorLogFileLocation())
-		status.PrintFailed()
-	}
-	if err != nil {
-		errorHandling.WithError(err).Panic(err)
-	}
-}
-
-func createPageForContent(siteId string, parentPageId string, contentID string, contentType string, relativeUrl string) {
-	os.Setenv("ParentPageContentTypeID", contentType)
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-	siteUseCase := csv.NewSiteUseCase(os.Getenv("AcousticAPIURL"))
-	createdPageID, err := siteUseCase.CreatePageForContent(siteId, parentPageId, contentID, relativeUrl)
-	log.Info("Page created with ID :" + createdPageID)
-	if err != nil {
-		errorHandling.WithError(err).Panic(err)
-	}
-}
-
-func clone(id string, sourceAcousticAuthApiHost string, sourceAcousticAPIKey string, targetAcousticAuthApiHost string, targetAcousticAPIKey string) {
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-	copyUseCase := csv.NewContentCopyUserCase(sourceAcousticAuthApiHost, sourceAcousticAPIKey, targetAcousticAuthApiHost, targetAcousticAPIKey)
-
-	_, err := copyUseCase.CopyContent(id, "_CL:"+time.Now().Format(time.ANSIC))
-	if err != nil {
-		errorHandling.WithError(err).Panic(err)
-	}
-	//log.Info(" total records :" + strconv.Itoa(contentStatus.TotalCount()))
-}
-
-func readContents(feedName string, configName string, acousticContentLib string, contentType string) {
-
-	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
-
-	contentService := csv.NewContentUseCase(os.Getenv("AcousticAPIURL"), acousticContentLib)
-	err := contentService.ReadBatch(contentType, feedName, configName)
-	if err != nil {
-		errorHandling.WithError(err).Panic(err)
-	}
-
-}
-
 func execute() {
 	log.Info("--------------Running Synky CLI----------------")
 	err := godotenv.Load()
@@ -135,137 +30,12 @@ func execute() {
 		log.Fatal("Error loading .env file")
 	}
 
-	feedLocation := flag.String("feedLocation", "", "File path of the feed")
-	configLocation := flag.String("configLocation", "", "File path of the config")
-	acousticLibraryID := flag.String("acousticLibraryID", "", "Acoustic Library ID")
-	contentTypeID := flag.String("contentTypeID", "", "Content Type ID")
-	contentOperation := flag.String("operation", "", "Operation")
-	deleteByFeed := flag.Bool("deleteByFeed", false, "Delete By Feed")
-	categoryName := flag.String("categoryName", "", "Root category name to add new categories")
-	deleteMappingName := flag.String("deleteMappingName", "", "Delete Mapping Name")
-	siteId := flag.String("siteID", "", "Site ID")
-	parentPageID := flag.String("parentPageID", "", "Parent page ID")
-	idToClone := flag.String("idToClone", "", "ID to clone")
-	contentIDForPage := flag.String("contentIDForPage", "", "Content ID to create page")
-	relativeUrlOfPage := flag.String("relativeUrlOfPage", "", "Relative URL of the page")
-	sourceAcousticAuthAPIHost := flag.String("sourceAcousticAPIUrl", "", "Source Acoustic  Host API")
-	sourceAcousticAPIKey := flag.String("sourceAcousticAPIKey", "", "Source Acoustic API Key")
-	targetAcousticAuthAPIHost := flag.String("targetAcousticAPIUrl", "", "Target Acoustic  Host API")
-	targetAcousticAPIKey := flag.String("targetAcousticAPIKey", "", "Target Acoustic API Key")
-	flag.Parse()
-
-	log.Info("feed location :" + *feedLocation)
-	log.Info("config location :" + *configLocation)
-	log.Info("Acoustic Library ID :" + *acousticLibraryID)
-	log.Info("Content type ID :" + *contentTypeID)
-	log.Info("Root Category Name :" + *categoryName)
-	log.Info("Operation :" + *contentOperation)
-	log.Info("Site ID :" + *siteId)
-	log.Info("Parent Page ID :" + *parentPageID)
-	log.Info("ID to clone :" + *idToClone)
-	log.Info("Content ID to create page :" + *contentIDForPage)
-	log.Info("Relative URL of the page :" + *relativeUrlOfPage)
-	log.Info("Source Acoustic Auth API Host :" + *sourceAcousticAuthAPIHost)
-	log.Info("Source Acoustic Auth API Key :" + *sourceAcousticAPIKey)
-	log.Info("Target Acoustic Auth API Host :" + *targetAcousticAuthAPIHost)
-	log.Info("Target Acoustic Auth API Key :" + *targetAcousticAPIKey)
-
-	if len(strings.TrimSpace(*contentOperation)) == 0 {
-		log.Error("Please provide the Content Operation (CREATE for create , UPDATE for update , READ for read) ")
-		os.Exit(1)
+	cmdProcessor := command.NewCommandActionProcessor()
+	errorHandling := logrus.PkgErrorEntry{Entry: log.WithField("", "")}
+	err = cmdProcessor.Execute()
+	if err != nil {
+		errorHandling.WithError(err).Panic(err)
 	}
-
-	if len(strings.TrimSpace(*feedLocation)) == 0 && *contentOperation != "CLONE_CONTENT" && *contentOperation != "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the feed location")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*configLocation)) == 0 && *contentOperation != "CLONE_CONTENT" && *contentOperation != "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the config location")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*acousticLibraryID)) == 0 && *contentOperation != "CREATE_CATEGORY" && *contentOperation != "CREATE_SITE_PAGE_FOR_CONTENT" && *contentOperation != "CLONE_CONTENT" {
-		log.Error("Please provide the Acoustic Library ID")
-		os.Exit(1)
-	} else {
-		os.Setenv("LibraryID", strings.TrimSpace(*acousticLibraryID))
-	}
-
-	if len(strings.TrimSpace(*contentTypeID)) == 0 && *contentOperation != "CREATE_CATEGORY" && *contentOperation != "CLONE_CONTENT" && *contentOperation != "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the Content Type ID")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*idToClone)) == 0 && *contentOperation == "CLONE_CONTENT" {
-		log.Error("Please provide the Content ID")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*sourceAcousticAuthAPIHost)) == 0 && *contentOperation == "CLONE_CONTENT" {
-		log.Error("Please provide the Source Acoustic auth API host")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*sourceAcousticAPIKey)) == 0 && *contentOperation == "CLONE_CONTENT" {
-		log.Error("Please provide the Source Acoustic auth API Key")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*targetAcousticAuthAPIHost)) == 0 && *contentOperation == "CLONE_CONTENT" {
-		log.Error("Please provide the Target Acoustic auth API host")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*targetAcousticAPIKey)) == 0 && *contentOperation == "CLONE_CONTENT" {
-		log.Error("Please provide the Target Acoustic auth API Key")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*contentIDForPage)) == 0 && *contentOperation == "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the Content ID")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*relativeUrlOfPage)) == 0 && *contentOperation == "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the Relative URL")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*siteId)) == 0 && *contentOperation == "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the Sit ID")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*parentPageID)) == 0 && *contentOperation == "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the Parent page ID")
-		os.Exit(1)
-	}
-
-	if len(strings.TrimSpace(*contentTypeID)) == 0 && *contentOperation == "CREATE_SITE_PAGE_FOR_CONTENT" {
-		log.Error("Please provide the Content Type")
-		os.Exit(1)
-	}
-
-	if *contentOperation == "CREATE" || *contentOperation == "UPDATE" {
-		createOrUpdateContents(*feedLocation, *configLocation, *acousticLibraryID, *contentTypeID)
-	} else if *contentOperation == "READ" {
-		readContents(*feedLocation, *configLocation, *acousticLibraryID, *contentTypeID)
-	} else if *contentOperation == "DELETE" {
-		deleteContents(*deleteByFeed, *deleteMappingName, *feedLocation, *configLocation, *acousticLibraryID, *contentTypeID)
-	} else if *contentOperation == "CREATE_CATEGORY" {
-		createCategories(*categoryName, *feedLocation, *configLocation)
-	} else if *contentOperation == "CREATE_SITE_PAGES" {
-		createSitePages(*siteId, *parentPageID, *contentTypeID, *feedLocation, *configLocation)
-	} else if *contentOperation == "CREATE_SITE_PAGE_FOR_CONTENT" {
-		createPageForContent(*siteId, *parentPageID, *contentIDForPage, *contentTypeID, *relativeUrlOfPage)
-	} else if *contentOperation == "CLONE_CONTENT" {
-		clone(*idToClone, *sourceAcousticAuthAPIHost, *sourceAcousticAPIKey, *targetAcousticAuthAPIHost, *targetAcousticAPIKey)
-	} else {
-		log.Error("Please provide the Content Operation (CREATE for create , UPDATE for update , READ for read , provided operation : {}", *contentOperation)
-		os.Exit(1)
-	}
-
 }
 
 func main() {
--- pkg/command/command.go
+++ pkg/command/command.go
@@ -1,6 +1,15 @@
 package command
 
-import "flag"
+import (
+	"flag"
+	"github.com/dekanayake/acoustic-content-sync/pkg/acoustic/csv"
+	"github.com/dekanayake/acoustic-content-sync/pkg/env"
+	"github.com/thoas/go-funk"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 import log "github.com/sirupsen/logrus"
 
 type Argument struct {
@@ -9,33 +18,12 @@ type Argument struct {
 	mandatory bool
 }
 
+type CommandHandler func(params map[string]*string) error
+
 type Command struct {
 	name      string
 	arguments []Argument
-}
-
-func (command *Command) ExtractAndValidateArguments() map[string]*string {
-	values := make(map[string]*string)
-	for _, arg := range command.arguments {
-		values[arg.name] = flag.String(arg.name, "", arg.usage)
-	}
-	flag.Parse()
-	isValid, validationErrorMessage := command.validateArguments(values)
-	if isValid {
-		return values
-	} else {
-		log.Error(validationErrorMessage)
-		return nil
-	}
-}
-
-func (command *Command) validateArguments(argValues map[string]*string) (bool, string) {
-	for _, arg := range command.arguments {
-		if arg.mandatory && *argValues[arg.name] == "" {
-			return false, "Please provide value for " + arg.name
-		}
-	}
-	return true, ""
+	handler   CommandHandler
 }
 
 type Action struct {
@@ -43,11 +31,120 @@ type Action struct {
 	argumentValues map[string]string
 }
 
-type CommandActionProcessor struct {
+func (command *Command) execute() error {
+	argumentValues := make(map[string]*string)
+	arguments := command.arguments
+	for _, argument := range arguments {
+		argValue := flag.String(argument.name, "", argument.usage)
+		flag.Parse()
+		if argument.mandatory && *argValue == "" {
+			log.Error("Please provide the " + argument.usage + ", usage: -" + argument.name + "=...")
+		}
+		log.Info(argument.usage + " : " + *argValue)
+		argumentValues[argument.name] = argValue
+	}
+	return command.handler(argumentValues)
+}
+
+type ActionProcessor struct {
 	commands []Command
 }
 
-func NewCommandActionProcessor() *CommandActionProcessor {
+func (processor *ActionProcessor) Execute() error {
+	contentOperation := flag.String("operation", "", "Operation")
+	flag.Parse()
+	if len(strings.TrimSpace(*contentOperation)) == 0 {
+		operations := funk.Reduce(processor.commands, func(acc string, cmd string) string {
+			return acc + cmd
+		}, "").(string)
+		log.Error("Please provide the Content Operation. Commands supported : " + operations)
+		os.Exit(1)
+	}
+
+	matchedContentOperationCmd := funk.Filter(processor.commands, func(command Command) bool {
+		return command.name == *contentOperation
+	})
+
+	if matchedContentOperationCmd == "" {
+		log.Error("Matching command not found for command :" + *contentOperation)
+		os.Exit(1)
+	}
+
+	command := matchedContentOperationCmd.(Command)
+	return command.execute()
+
+}
+
+func createOrUpdateContents(feedName string, configName string, acousticContentLib string, contentType string) error {
+	contentService := csv.NewContentUseCase(os.Getenv("AcousticAPIURL"), acousticContentLib)
+	status, err := contentService.CreateBatch(contentType, feedName, configName)
+	log.Info(" total records :" + strconv.Itoa(status.TotalCount()))
+	log.Info(" success created record count  :" + strconv.Itoa(len(status.Success)))
+	if status.FailuresExist() {
+		log.Error("There are " + strconv.Itoa(len(status.Failed)) + " failures in creating contents , please check the log in " + env.ErrorLogFileLocation())
+		status.PrintFailed()
+	}
+	return err
+}
+
+func deleteContents(deleteUsingFeed bool, deleteMappingName string, feedName string, configName string, libraryID string, contentType string) error {
+	deleteService := csv.NewDeleteService(env.AcousticAPIUrl())
+	if deleteUsingFeed {
+		status, err := deleteService.DeleteByFeed(deleteMappingName, contentType, feedName, configName)
+		log.Info(" total records :" + strconv.Itoa(status.TotalCount()))
+		log.Info(" success deleted record count  :" + strconv.Itoa(len(status.Success)))
+		if status.FailuresExist() {
+			log.Error("There are " + strconv.Itoa(len(status.Failed)) + " failures in creating contents , please check the log in " + env.ErrorLogFileLocation())
+			status.PrintFailed()
+		}
+		return err
+	} else {
+		err := deleteService.Delete(libraryID, deleteMappingName, configName)
+		return err
+	}
+}
+
+func createCategories(catName string, feedName string, configName string) error {
+	catService := csv.NewCategoryService(os.Getenv("AcousticAPIURL"))
+	err := catService.Create(catName, feedName, configName)
+	return err
+}
+
+func createSitePages(siteId string, parentPageId string, contentType string, dataFeedPath string, configPath string) error {
+	os.Setenv("ParentPageContentTypeID", contentType)
+	siteUseCase := csv.NewSiteUseCase(os.Getenv("AcousticAPIURL"))
+	status, err := siteUseCase.CreatePages(siteId, parentPageId, contentType, dataFeedPath, configPath)
+	log.Info(" total records :" + strconv.Itoa(status.TotalCount()))
+	log.Info(" success created pages count  :" + strconv.Itoa(len(status.Success)))
+	if status.FailuresExist() {
+		log.Error("There are " + strconv.Itoa(len(status.Failed)) + " failures in creating pages , please check the log in " + env.ErrorLogFileLocation())
+		status.PrintFailed()
+	}
+	return err
+}
+
+func createPageForContent(siteId string, parentPageId string, contentID string, contentType string, relativeUrl string) error {
+	os.Setenv("ParentPageContentTypeID", contentType)
+	siteUseCase := csv.NewSiteUseCase(os.Getenv("AcousticAPIURL"))
+	createdPageID, err := siteUseCase.CreatePageForContent(siteId, parentPageId, contentID, relativeUrl)
+	log.Info("Page created with ID :" + createdPageID)
+	return err
+}
+
+func clone(id string, sourceAcousticAuthApiHost string, sourceAcousticAPIKey string, targetAcousticAuthApiHost string, targetAcousticAPIKey string) error {
+	copyUseCase := csv.NewContentCopyUserCase(sourceAcousticAuthApiHost, sourceAcousticAPIKey, targetAcousticAuthApiHost, targetAcousticAPIKey)
+
+	_, err := copyUseCase.CopyContent(id, "_CL:"+time.Now().Format(time.ANSIC))
+	return err
+}
+
+func readContents(feedName string, configName string, acousticContentLib string, contentType string) error {
+	contentService := csv.NewContentUseCase(os.Getenv("AcousticAPIURL"), acousticContentLib)
+	err := contentService.ReadBatch(contentType, feedName, configName)
+	return err
+}
+
+func NewCommandActionProcessor() *ActionProcessor {
 	createCommand := Command{
 		name: "CREATE",
 		arguments: []Argument{
@@ -71,6 +168,9 @@ func NewCommandActionProcessor() *CommandActionProcessor {
 				usage:     "Content Type ID",
 				mandatory: true,
 			},
+		},
+		handler: func(params map[string]*string) error {
+			return createOrUpdateContents(*params["feedLocation"], *params["configLocation"], *params["acousticLibraryID"], *params["contentTypeID"])
 		},
 	}
 
@@ -98,6 +198,9 @@ func NewCommandActionProcessor() *CommandActionProcessor {
 				mandatory: true,
 			},
 		},
+		handler: func(params map[string]*string) error {
+			return createOrUpdateContents(*params["feedLocation"], *params["configLocation"], *params["acousticLibraryID"], *params["contentTypeID"])
+		},
 	}
 
 	readCommand := Command{
@@ -124,9 +227,138 @@ func NewCommandActionProcessor() *CommandActionProcessor {
 				mandatory: true,
 			},
 		},
+		handler: func(params map[string]*string) error {
+			return readContents(*params["feedLocation"], *params["configLocation"], *params["acousticLibraryID"], *params["contentTypeID"])
+		},
 	}
 
-	return &CommandActionProcessor{
-		commands: []Command{createCommand, updateCommand, readCommand},
+	createCategoryCommand := Command{
+		name: "CREATE_CATEGORY",
+		arguments: []Argument{
+			Argument{
+				name:      "categoryName",
+				usage:     "Root category name to add new categories",
+				mandatory: true,
+			},
+			Argument{
+				name:      "feedLocation",
+				usage:     "File path of the feed",
+				mandatory: true,
+			},
+			Argument{
+				name:      "configLocation",
+				usage:     "File path of the config",
+				mandatory: true,
+			},
+		},
+		handler: func(params map[string]*string) error {
+			return createCategories(*params["categoryName"], *params["feedLocation"], *params["configLocation"])
+		},
+	}
+
+	createSitePages := Command{
+		name: "CREATE_SITE_PAGES",
+		arguments: []Argument{
+			Argument{
+				name:      "siteId",
+				usage:     "Site ID",
+				mandatory: true,
+			},
+			Argument{
+				name:      "parentPageID",
+				usage:     "Parent page ID",
+				mandatory: true,
+			},
+			Argument{
+				name:      "contentTypeID",
+				usage:     "Content Type ID",
+				mandatory: true,
+			},
+			Argument{
+				name:      "feedLocation",
+				usage:     "File path of the feed",
+				mandatory: true,
+			},
+			Argument{
+				name:      "configLocation",
+				usage:     "File path of the config",
+				mandatory: true,
+			},
+		},
+		handler: func(params map[string]*string) error {
+			return createSitePages(*params["siteId"], *params["parentPageID"], *params["contentTypeID"], *params["feedLocation"], *params["configLocation"])
+		},
+	}
+
+	createSitePageForContent := Command{
+		name: "CREATE_SITE_PAGE_FOR_CONTENT",
+		arguments: []Argument{
+			Argument{
+				name:      "siteId",
+				usage:     "Site ID",
+				mandatory: true,
+			},
+			Argument{
+				name:      "parentPageID",
+				usage:     "Parent page ID",
+				mandatory: true,
+			},
+			Argument{
+				name:      "contentIDForPage",
+				usage:     "Content ID to create page",
+				mandatory: true,
+			},
+			Argument{
+				name:      "contentTypeID",
+				usage:     "Content Type ID",
+				mandatory: true,
+			},
+			Argument{
+				name:      "relativeUrlOfPage",
+				usage:     "Relative URL of the page",
+				mandatory: true,
+			},
+		},
+		handler: func(params map[string]*string) error {
+			return createPageForContent(*params["siteId"], *params["parentPageID"], *params["contentIDForPage"], *params["contentTypeID"], *params["relativeUrlOfPage"])
+		},
+	}
+
+	cloneContent := Command{
+		name: "CLONE_CONTENT",
+		arguments: []Argument{
+			Argument{
+				name:      "idToClone",
+				usage:     "ID to clone",
+				mandatory: true,
+			},
+			Argument{
+				name:      "sourceAcousticAuthAPIHost",
+				usage:     "Source Acoustic  Host API",
+				mandatory: true,
+			},
+			Argument{
+				name:      "sourceAcousticAPIKey",
+				usage:     "Source Acoustic API Key",
+				mandatory: true,
+			},
+			Argument{
+				name:      "targetAcousticAuthAPIHost",
+				usage:     "Target Acoustic  Host API",
+				mandatory: true,
+			},
+			Argument{
+				name:      "targetAcousticAPIKey",
+				usage:     "Target Acoustic API Key",
+				mandatory: true,
+			},
+		},
+		handler: func(params map[string]*string) error {
+			return clone(*params["idToClone"], *params["sourceAcousticAuthAPIHost"], *params["sourceAcousticAPIKey"], *params["targetAcousticAuthAPIHost"], *params["targetAcousticAPIKey"])
+		},
+	}
+
+	return &ActionProcessor{
+		commands: []Command{createCommand, updateCommand, readCommand, createCategoryCommand, createSitePages, createSitePageForContent, cloneContent},
 	}
 }
